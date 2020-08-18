4a9ccde0ac01aa3d4897e23ac006469829cf084c
Erik Narhi <Erik.Narhi@burohappold.com>
2020-08-18 09:08:35 -0700

    Namespace update

--- CarbonQueryDatabase_oM/Adapter/CQDConfig.cs
+++ CarbonQueryDatabase_oM/Adapter/CQDConfig.cs
@@ -24,7 +24,7 @@ using BH.oM.Adapter;
 using System.Collections.Generic;
 using System.ComponentModel;
 
-namespace BH.oM.Adapter.CarbonQueryDatabase
+namespace BH.oM.Adapters.CarbonQueryDatabase
 {
     [Description("This Config can be specified in the `ActionConfig` input of any Adapter Action (e.g. Push).")]
     // Note: this will get passed within any CRUD method (see their signature). 
