85ae8190e0b0009f406320d9b9be6e256c343bfd
Michael Hoehn <michael.hoehn@burohappold.com>
2020-10-27 17:06:26 -0400

    Update attributes

--- CarbonQueryDatabase_Engine/Create/GetRequest.cs
+++ CarbonQueryDatabase_Engine/Create/GetRequest.cs
@@ -38,7 +38,6 @@ namespace BH.Engine.Adapters.CarbonQueryDatabase
         [Description("Create a GetRequest for the CarbonQueryDatabase")]
         [Input("apiCommand", "The CarbonQueryDatabase REST API command to create a GetRequest with")]
         [Input("bearerToken", "The CarbonQueryDatabase bearerToken (this can be acquired using Compute BearerToken with your EC3 username and password)")]
-        [Input("parameters", "An optional CustomObject with properties representing parameters to create the GetRequest with (ie count, name_like, etc)")]
         [Output("GetRequest", "A GetRequest with CarbonQueryDatabase specific headers and uri")]
 
         public static GetRequest CarbonQueryDatabaseRequest(string apiCommand, string bearerToken)
@@ -49,10 +48,9 @@ namespace BH.Engine.Adapters.CarbonQueryDatabase
                 Headers = new Dictionary<string, object>()
                 {
                     { "Authorization", "Bearer " +  bearerToken }
-                },
-        };
+                }
+            };
         }
-
         /***************************************************/
     }
 }
