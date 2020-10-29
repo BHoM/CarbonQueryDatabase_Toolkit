d5533ffef77b1a5af2c247c0fd173bd60c711191
Michael Hoehn <michael.hoehn@burohappold.com>
2020-10-29 11:48:12 -0400

    Remove pointless fragment

--- CarbonQueryDatabase_Adapter/CRUD/Read.cs
+++ CarbonQueryDatabase_Adapter/CRUD/Read.cs
@@ -35,7 +35,6 @@ using BH.oM.LifeCycleAssessment.MaterialFragments;
 using BH.Adapter;
 using BH.Engine.Serialiser;
 using BH.Engine.Reflection;
-using BH.oM.Adapters.CarbonQueryDatabase.Fragments;
 
 namespace BH.Adapter.CarbonQueryDatabase
 {
