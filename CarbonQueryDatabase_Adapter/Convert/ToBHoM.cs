1496460c384f6e5f78e3b77a53b9b0eaee0fb68c
Michael Hoehn <michael.hoehn@burohappold.com>
2021-05-14 09:51:07 -0600

    Updates to support EPD refactor

--- CarbonQueryDatabase_Adapter/Convert/ToBHoM.cs
+++ CarbonQueryDatabase_Adapter/Convert/ToBHoM.cs
@@ -20,23 +20,19 @@
  * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
  */
 
+using BH.Engine.Reflection;
+using BH.Engine.Units;
+using BH.oM.Base;
+using BH.oM.LifeCycleAssessment;
+using BH.oM.LifeCycleAssessment.Fragments;
+using BH.oM.LifeCycleAssessment.MaterialFragments;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
-using System.Text;
-using System.Threading.Tasks;
-using System.Net;
-using System.Net.Http;
-using System.ComponentModel;
-using BH.oM.Reflection.Attributes;
-using BH.oM.Base;
-using BH.oM.LifeCycleAssessment;
-using BH.oM.LifeCycleAssessment.MaterialFragments;
-using BH.Engine.Reflection;
-using System.Collections;
-using BH.Engine.Adapters.CarbonQueryDatabase;
-using BH.Engine.Units;
+
+using BH.oM.Adapters.CarbonQueryDatabase;
 
 namespace BH.Adapter.CarbonQueryDatabase
 {
@@ -46,7 +42,7 @@ namespace BH.Adapter.CarbonQueryDatabase
         /****           Public Methods                  ****/
         /***************************************************/
 
-        public static EnvironmentalProductDeclaration ToEnvironmentalProductDeclaration(this CustomObject obj)
+        public static EnvironmentalProductDeclaration ToEnvironmentalProductDeclaration(this CustomObject obj, CQDConfig config)
         {
             int result = 0;
 
@@ -66,81 +62,83 @@ namespace BH.Adapter.CarbonQueryDatabase
             double density = ConvertToSI(densityVal, densityUnits);
             string gwp = obj.PropertyValue("gwp")?.ToString() ?? "";
             double gwpVal = (gwp == "") ? double.NaN : System.Convert.ToDouble(gwp.Substring(0, gwp.IndexOf(" "))) * epdUnitMult;
+            int lifespan = (int)(obj.PropertyValue("reference_service_life") ?? 0);
+            int referenceYear = int.TryParse(obj.PropertyValue("date_of_issue")?.ToString() ?? "", out result) ? result : 0;
 
-            EnvironmentalProductDeclaration epd = new EnvironmentalProductDeclaration
-            {
-                QuantityType = QuantityType.Volume,
-                Id = obj.PropertyValue("id")?.ToString() ?? "",
-                Name = obj.PropertyValue("name")?.ToString() ?? "",
-                Manufacturer = obj.PropertyValue("manufacturer.name")?.ToString() ?? "",
-                Plant = obj.PropertyValue("plant.name")?.ToString() ?? "",
-                PostalCode = int.TryParse(obj.PropertyValue("plant.postal_code")?.ToString() ?? "", out result) ? result : 0,
-                Density = density,
-                GlobalWarmingPotential = gwpVal,
-                BiogenicCarbon = obj.PropertyValue("biogenic_embodied_carbon") != null ? System.Convert.ToDouble(obj.PropertyValue("biogenic_embodied_carbon_z")) * epdUnitMult : double.NaN,
-                Description = obj.PropertyValue("description")?.ToString() ?? "",
-                IndustryStandards = standards != null  ? standards.ToList() : new List<string>(),
-            };
-                                
-            return epd;
-        }
-
-        /***************************************************/
-
-        public static SectorEnvironmentalProductDeclaration ToSectorEnvironmentalProductDeclaration(this CustomObject obj)
-        {
-            List<string> publisherNames = new List<string>();
+            string publisherNames = "";
             if (obj.PropertyValue("publishers") != null)
             {
                 IEnumerable pubs = (IEnumerable)obj.PropertyValue("publishers");
                 foreach (CustomObject pub in pubs)
                 {
-                    publisherNames.Add(pub.PropertyValue("name").ToString());
+                    publisherNames += pub.ToString() + " ";
                 }
+                publisherNames = publisherNames.Trim();
             }
 
-            List<string> jurisdictionNames = new List<string>();
+            string jurisdictionNames = "";
             if (obj.PropertyValue("geography") != null)
             {
                 IEnumerable jurs = (IEnumerable)obj.PropertyValue("geography.country_codes");
                 foreach (object jur in jurs)
                 {
-                   jurisdictionNames.Add(jur.ToString());
+                    jurisdictionNames += jur.ToString() + " ";
                 }
+                jurisdictionNames = jurisdictionNames.Trim();
             }
 
-            string declaredUnit = obj.PropertyValue("declared_unit")?.ToString() ?? "";
-            string epdUnit = GetUnitsFromString(declaredUnit);
-            double declaredVal = GetValFromString(declaredUnit);
-            QuantityType quantityType = GetQuantityTypeFromString(epdUnit);
-            double epdUnitMult = ConvertToSI(1/declaredVal, epdUnit);
-
-            string densityString = obj.PropertyValue("density_max")?.ToString() ?? "";
-            double densityVal = GetValFromString(densityString);
-            string densityUnits = GetUnitsFromString(densityString);
-            double density = ConvertToSI(densityVal, densityUnits);
-            string gwp = obj.PropertyValue("gwp")?.ToString() ?? "";
-            double gwpVal = (gwp == "") ? double.NaN : System.Convert.ToDouble(gwp.Substring(0, gwp.IndexOf(" "))) * epdUnitMult;
-
-            SectorEnvironmentalProductDeclaration epd = new SectorEnvironmentalProductDeclaration
+            EnvironmentalMetric metric = new EnvironmentalMetric
             {
-                QuantityType = quantityType,
-                Id = obj.PropertyValue("id")?.ToString() ?? "",
-                Name = obj.PropertyValue("name")?.ToString() ?? "",
-                Density = density,
-                GlobalWarmingPotential = gwpVal,
-                BiogenicCarbon = obj.PropertyValue("biogenic_embodied_carbon") != null ? System.Convert.ToDouble(obj.PropertyValue("biogenic_embodied_carbon_z")) * epdUnitMult : double.NaN,
-                Description = obj.PropertyValue("description")?.ToString() ?? "",
-                Jurisdiction = jurisdictionNames,
-                Publisher = publisherNames,
+                Field = EnvironmentalProductDeclarationField.GlobalWarmingPotential,
+                Phases = new List<LifeCycleAssessmentPhases>() { LifeCycleAssessmentPhases.A1, LifeCycleAssessmentPhases.A2, LifeCycleAssessmentPhases.A3},
+                Quantity = gwpVal,
             };
 
-            return epd;
+            AdditionalEPDData data = new AdditionalEPDData
+            {
+                Description = obj.PropertyValue("description")?.ToString() ?? "",
+                EndOfLifeTreatment = "",
+                Id = obj.PropertyValue("id")?.ToString() ?? "",
+                IndustryStandards = standards != null ? standards.ToList() : new List<string>(),
+                Jurisdiction = jurisdictionNames,
+                LifeSpan = lifespan,
+                Manufacturer = obj.PropertyValue("manufacturer.name")?.ToString() ?? "",
+                PlantName = obj.PropertyValue("plant.name")?.ToString() ?? "",
+                PostalCode = int.TryParse(obj.PropertyValue("plant.postal_code")?.ToString() ?? "", out result) ? result : 0,
+                Publisher = publisherNames,
+                ReferenceYear = referenceYear,
+            };
+
+            EPDDensity densityFragment = new EPDDensity
+            {
+                Density = density,
+            };
+
+            EnvironmentalProductDeclaration epd = new EnvironmentalProductDeclaration
+            {
+                Type = config.Type,
+                EnvironmentalMetric = new List<EnvironmentalMetric> { metric }, 
+                QuantityType = quantityType,
+                QuantityTypeValue = 1,
+                Name = obj.PropertyValue("name")?.ToString() ?? "",
+            };
+
+            // Add Additional Data Fragment
+            EnvironmentalProductDeclaration epdData = (EnvironmentalProductDeclaration)Engine.Base.Modify.AddFragment(epd, data);
+            
+            // Add Density Fragment
+            if (density != 0)
+            {
+                EnvironmentalProductDeclaration epdDataDensity = (EnvironmentalProductDeclaration)Engine.Base.Modify.AddFragment(epdData, densityFragment);
+                return epdDataDensity;
+            }
+            else
+            {
+                return epdData;
+            }
         }
 
         /***************************************************/
-
-
 
         public static QuantityType GetQuantityTypeFromString(string unitFrom)
         {    
