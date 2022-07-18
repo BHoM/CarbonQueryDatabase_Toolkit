570870e62b06362f4c3da267280c20de5da2072a
FraserGreenroyd <fraser.greenroyd@burohappold.com>
2022-07-18 17:38:07 +0100

    Update Assembly Information

--- CarbonQueryDatabase_Adapter/Properties/AssemblyInfo.cs
+++ CarbonQueryDatabase_Adapter/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CarbonQueryDatabase_Adapter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("https://github.com/BHoM/CarbonQueryDatabase_Toolkit")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CarbonQueryDatabase_Adapter")]
